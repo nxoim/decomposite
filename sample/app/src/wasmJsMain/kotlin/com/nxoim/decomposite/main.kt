2a32c40836097673580674addd72a9ec739a1cc5
nxoim <ghite@nxoim.com>
2024-08-05 05:05:27 +0200

    edits

--- sample/app/src/wasmJsMain/kotlin/com/nxoim/decomposite/main.kt
+++ sample/app/src/wasmJsMain/kotlin/com/nxoim/decomposite/main.kt
@@ -1,40 +1,18 @@
 package com.nxoim.decomposite
 
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.window.CanvasBasedWindow
-import com.nxoim.decomposite.core.common.navigation.CommonNavigationRootProvider
-import com.nxoim.decomposite.core.common.navigation.InternalNavigationRootApi
-import com.nxoim.decomposite.core.common.navigation.NavigationRoot
+import com.nxoim.decomposite.core.common.navigation.FallbackNavigationRootImplementation
 import com.nxoim.decomposite.core.common.navigation.NavigationRootData
-import com.nxoim.decomposite.core.common.ultils.ScreenInformation
-import com.nxoim.decomposite.core.common.ultils.ScreenShape
-import kotlin.math.roundToInt
+import com.nxoim.decomposite.core.common.navigation.NavigationRootProvider
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, FallbackNavigationRootImplementation::class)
 fun main() {
-	val navigationRootData = NavigationRootData()
+    val navigationRootData = NavigationRootData()
 
-	CanvasBasedWindow(canvasElementId = "ComposeTarget") {
-		BoxWithConstraints {
-			val screenInformation = ScreenInformation(
-				widthPx = (this.maxWidth * LocalDensity.current.density).value.roundToInt(),
-				heightPx = (this.maxHeight * LocalDensity.current.density).value.roundToInt(),
-				screenShape = ScreenShape(
-					path = null,
-					corners = null
-				)
-			)
-
-			@OptIn(InternalNavigationRootApi::class)
-			CommonNavigationRootProvider(
-				remember { NavigationRoot(screenInformation) },
-				navigationRootData
-			) {
-                App()
-			}
-		}
-	}
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+        NavigationRootProvider(navigationRootData) {
+            App()
+        }
+    }
 }
