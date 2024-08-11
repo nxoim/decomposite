991b2382c9e5d31f1a3c37ebe47f20b5373eabd3
nxoim <ghite@nxoim.com>
2024-08-11 17:21:37 +0200

    ContentAnimatorScope: updated documentation

--- core/src/commonMain/kotlin/com/nxoim/decomposite/core/common/navigation/animations/scopes/ContentAnimatorScope.kt
+++ core/src/commonMain/kotlin/com/nxoim/decomposite/core/common/navigation/animations/scopes/ContentAnimatorScope.kt
@@ -8,34 +8,56 @@ import com.nxoim.decomposite.core.common.ultils.BackGestureEvent
 
 /**
  * Base for the content animator scope implementations. Describes the bare minimum
- * needed. When implementing - keep in mind:
- * - elements, appearing and disappearing from the stack, update the state and trigger animations
+ * needed.
  *
- * - [onBackGesture] represents the user's actions and should not be used to manipulate the stack
- *
- * - [indexFromTop] represents the index of the item from the top of the stack, with
- * 0 being the top. Negative numbers represent an item not existing in the stack while being
- * animated. If several items are being removed and all are animated at the same time - 
- * [indexFromTop] will be represent the order of the items being removed, -1 being
- * the latest item that has been removed. Yes, the number can be less than -1.
- *
- * [animationProgressForScope] is a value that is used to provide [AnimatedVisibilityScope]'s
- * to the content for things like modifiers that depend on [SharedTransitionScope].
- * The resulting value provided to the [AnimatedVisibilityScope] is constrained to a
- * range between 0f and 1f. It's expected to mirror [indexFromTop], meaning it must
- * be -1 when the item is outside of the stack, 0 when at the top of the stack, and 1 when
- * at the back of the stack. It's also expected to depend on back gestures.
- *
- * Note: when several animations with different keys are used for a single item -
+ * When several animations with different keys are used for a single item -
  * the first scope is used to provide [animationProgressForScope]. Refer to
  * [ContentAnimator] and [contentAnimator] for more information.
  */
 interface ContentAnimatorScope {
-    val indexFromTop: Int
-    val index: Int
-    val animationStatus: AnimationStatus
-    val animationProgressForScope: Float
+	/**
+	 * Represents the index of the item from the top of the stack, with
+	 * 0 being the top. Negative numbers represent an item not existing in the stack while being
+	 * animated.
+	 *
+	 * If several items are being removed and all are animated at the same time -
+	 * [indexFromTop] will represent the order of the items being removed, -1 being
+	 * the latest item that has been removed.
+	 */
+	val indexFromTop: Int
 
-    suspend fun onBackGesture(backGesture: BackGestureEvent): Any
-    suspend fun update(newIndex: Int, newIndexFromTop: Int)
+	/**
+	 * Represents the index of the item in the stack.
+	 *
+	 * If several items are being removed and all are animated at the same time -
+	 * [index] will represent the order of the items being removed, -1 being
+	 * the latest item that has been removed.
+	 */
+	val index: Int
+
+	/**
+	 * Represents the current animation status.
+	 */
+	val animationStatus: AnimationStatus
+
+	/**
+	 * A value that is used to provide [AnimatedVisibilityScope]'s to the content
+	 * for things like modifiers that depend on [SharedTransitionScope].
+	 * The resulting value provided to the [AnimatedVisibilityScope] is constrained to a
+	 * range between 0f and 1f. It's expected to mirror [indexFromTop], meaning it must
+	 * be -1 when the item is outside of the stack, 0 when at the top of the stack, and 1 when
+	 * at the back of the stack. It's also expected to depend on back gestures.
+	 */
+	val animationProgressForScope: Float
+
+	/**
+	 * Represents the user's actions and should not⚠️ be used to manipulate the stack
+	 */
+	suspend fun onBackGesture(backGesture: BackGestureEvent): Any
+
+	/**
+	 * Updates the animation data in the scope and starts animations.
+	 */
+	suspend fun update(newIndex: Int, newIndexFromTop: Int)
 }
+
